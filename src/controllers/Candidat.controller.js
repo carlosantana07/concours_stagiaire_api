201627578770bdf85507e9fe9eb20da9ccee3b0f
Yobi <yobibah7295@gmail.com>
2026-04-29 15:41:26 +0000

    mak

--- src/controllers/Candidat.controller.js
+++ src/controllers/Candidat.controller.js
@@ -45,7 +45,7 @@ export class CandidatController {
       },
     });
 
-     candidat.date_naissance = candidat.date_naissance.toLocaleDateString("FR-fr")
+     candidat.date_naissance = candidat.date_naissance.toLocaleDateString("fr-FR")
 
     if (!candidat) {
       return res.status(404).json({ error: "Candidat introuvable" });
