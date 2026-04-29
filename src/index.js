1e8936026bde764f69d3e3007cb1aa9a139e6213
Yobi <yobibah7295@gmail.com>
2026-04-29 14:51:54 +0000

    supression trycatch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
+import '../src/middleware/routerPatch.js'
 import express from "express";
 import candidatRoutes from "./routes/candidat.route.js";
 import authRoutes from "./routes/auth.route.js";
-import adminRoutes from "./routes/admin.route.js"; 
+import adminRoutes from "./routes/admin.route.js";
 import cors from "cors";
 import { swaggerDocs } from "./swagger.js";
 import helmet from "helmet";
@@ -13,46 +14,25 @@ import inscriptionRoutes from "./routes/inscription.route.js";
 import paiementRoutes from "./routes/paiement.route.js";
 import concoursRoutes from "./routes/concours.route.js";
 import { limiter } from "./middleware/rateLimiter.js";
-import { Cron } from "./cron/Cron.js";
+
 import { ensureBucketExists } from "./config/minio.js";
-
+import { UpdateStatusConcours } from './cron/Cron.js';
 const app = express();
-
 const PORT = process.env.PORT || 4000;
 
-// const allowedOrigins = [
-//   "http://localhost:3000",   
-//   "http://localhost:4000",  
-//   "http://localhost:5000",    
-//   "http://localhost:6000", 
-// ];
-
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true);
-
-//     if (allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("CORS non autorisé"));
-//     }
-//   },
-//   credentials: true,
-// };
 await ensureBucketExists('e-concours');
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use(limiter)
+app.use(limiter);
 
 app.use("/api/inscription", inscriptionRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/candidat", candidatRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api/payment",paiementRoutes)
-app.use("/api/concours",concoursRoutes)
-
+app.use("/api/payment", paiementRoutes);
+app.use("/api/concours", concoursRoutes);
 
 swaggerDocs(app, PORT);
 
@@ -60,11 +40,15 @@ app.get("/", (req, res) => {
   res.send("API e-concours opérationnelle");
 });
 
-// await connection.set("test", "ioredis fonctionne !");
-// const val = await connection.get("test");
-// console.log(val); // ioredis fonctionne !
+// Middleware d'erreur global  ici les erreurs 500 sont renvonyer genre un try  global
+app.use((err, req, res, next) => {
+  console.error('Erreur serveur :', err.message);
+  return res.status(500).json({ error: 'Une erreur interne est survenue' });
+});
+
+
+UpdateStatusConcours();
 
 app.listen(PORT, () => {
-
   console.log(`Server is running on port ${PORT}`);
 });
