e1bd1a699434d16c78651efc206591b382c5e7ce
huguette11 <estellehuguetteyogo@gmail.com>
2026-05-18 14:53:30 +0000

    gf

--- src/index.js
+++ src/index.js
@@ -14,13 +14,14 @@ import inscriptionRoutes from "./routes/inscription.route.js";
 import paiementRoutes from "./routes/paiement.route.js";
 import concoursRoutes from "./routes/concours.route.js";
 import { limiter } from "./middleware/rateLimiter.js";
+import examenRoutes from "./routes/examen.route.js";
 
 import { ensureBucketExists } from "./config/minio.js";
 import { UpdateStatusConcours } from './cron/Cron.js';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-await ensureBucketExists('e-concours');
+// await ensureBucketExists('e-concours');
 
 app.use(cors());
 app.use(helmet());
@@ -33,6 +34,7 @@ app.use("/api/candidat", candidatRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/payment", paiementRoutes);
 app.use("/api/concours", concoursRoutes);
+app.use("/api/examen", examenRoutes);
 
 swaggerDocs(app, PORT);
 
