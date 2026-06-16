380753679bee36b8324fae5d6ca1ea79ece51464
Yobi <yobibah7295@gmail.com>
2026-06-16 10:37:39 +0000

    correction de la config de redi

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,9 +1,10 @@
-import { Redis } from "ioredis";
+import Redis  from "ioredis";
 
-export const connection = new Redis({
+const connection = new Redis({
   host: "127.0.0.1",
   port: 6379,
 });
+export default connection;
 
 connection.on("connect", () => console.log(" Redis connecté"));
 connection.on("error", (err) => console.error("Redis erreur :", err));
